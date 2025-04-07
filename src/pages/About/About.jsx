9bf78dad26794528b3b68dc9ff0462bf1221ece8
Àlex Meléndez <alex27mel@gmail.com>
2025-04-07 15:21:07 +0200

    .
    .

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className='about-section'>
       <h1 className='about-title'>About</h1>
-      <h2 className='about-text'>Hi I'm Àlex Meléndez Centeno <br />and this website is my first project made with React for my full stack master degre offered by ThePower Education</h2>
+      <h2 className='about-text'>Hello, my name is Àlex Meléndez Centeno, <br /> and I developed this website as my first project using React, as part of the Full Stack Master's Program offered by ThePower Education.</h2>
     </div>
   )
 }
