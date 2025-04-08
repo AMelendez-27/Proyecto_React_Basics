853fe1216dd1bee5468cfba91fb1415ca5dc60bc
Àlex Meléndez <alex27mel@gmail.com>
2025-04-08 08:47:31 +0200

    .
    .

--- src/components/CompactHeader/CompactHeader.jsx
+++ src/components/CompactHeader/CompactHeader.jsx
@@ -1,13 +1,20 @@
 import './CompactHeader.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 const CompactHeader = () => {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
   const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setIsMenuVisible((prevState) => !prevState);
+  };
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuVisible(false);
+    }
   };
 
   useEffect(() => {
@@ -21,30 +28,31 @@ const CompactHeader = () => {
     };
 
     handleResize();
-
     window.addEventListener('resize', handleResize);
+    document.addEventListener('click', handleClickOutside);
 
     return () => {
       window.removeEventListener('resize', handleResize);
+      document.removeEventListener('click', handleClickOutside);
     };
   }, []);
 
   return (
     isHeaderVisible && (
-      <header className={`compact-menu ${isMenuVisible ? 'resize-menu' : ''}`}>
+      <header ref={menuRef} className={`compact-menu ${isMenuVisible ? 'resize-menu' : ''}`}>
         <img className="menu-icon" src="/assets/menu-icon.png" alt="Menu icon" onClick={toggleMenu} />
         <nav className={`compact-nav ${isMenuVisible ? '' : 'hidden-menu'}`}>
           <ul className='compact-menu-list'>
-            <li className='compact-menu-item'>
+            <li className='compact-menu-item' onClick={() => setIsMenuVisible(false)}>
               <Link to="/">Home</Link>
             </li>
-            <li className='compact-menu-item'>
+            <li className='compact-menu-item' onClick={() => setIsMenuVisible(false)}>
               <Link to="/browse-pokemon">Latest Set</Link>
             </li>
-            <li className='compact-menu-item'>
+            <li className='compact-menu-item' onClick={() => setIsMenuVisible(false)}>
               <Link to="/explore-sets">Explore sets</Link>
             </li>
-            <li className='compact-menu-item'>
+            <li className='compact-menu-item' onClick={() => setIsMenuVisible(false)}>
               <Link to="/about">About</Link>
             </li>
           </ul>
